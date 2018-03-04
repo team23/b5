bf36683169054c6277e93a2bb691f2d157590284
David Danier <danier@team23.de>
2018-03-04 14:02:59 +0100

    Started next dev version

--- b5/__init__.py
+++ b5/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-VERSION = '0.11.5'
+VERSION = '0.11.6-dev'
 B5_PATH = os.path.realpath(os.path.dirname(__file__))
 B5_BASH_PATH = os.path.join(B5_PATH, 'bash')
 B5_TEMPLATES_PATH = os.path.join(B5_PATH, 'templates')

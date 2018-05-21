61ac59300ab58961569270ad9f40242c654395fe
David Danier <danier@team23.de>
2018-05-21 16:52:19 +0200

    Release 0.11.10

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='b5',
-    version='0.11.9',
+    version='0.11.10',
     description='b5 - simple and sane task runner',
     author='David Danier',
     author_email='danier@team23.de',

986fa91c28789e756a200c573b5fb50123780593
David Danier <danier@team23.de>
2019-04-07 17:45:07 +0200

    Release 1.1.2

--- b5/__init__.py
+++ b5/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-VERSION = '1.1.2-dev'
+VERSION = '1.1.2'
 B5_PATH = os.path.realpath(os.path.dirname(__file__))
 B5_BASH_PATH = os.path.join(B5_PATH, 'bash')
 B5_TEMPLATES_PATH = os.path.join(B5_PATH, 'templates')

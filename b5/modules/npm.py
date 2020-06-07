8da3efc6bd640bc53e5060282ee1c581a4ac9b62
Emanuel Jacob <jacob@team23.de>
2020-06-07 23:07:33 +0200

    feat(): Add npm version check

--- b5/modules/npm.py
+++ b5/modules/npm.py
@@ -1,5 +1,7 @@
+import re
 import shlex
 import os
+import subprocess
 
 from . import BaseModule
 
@@ -12,6 +14,9 @@ class NpmModule(BaseModule):
         'base_path': '.',
         'npm_bin': 'npm',
     }
+
+    def get_version(self):
+        return subprocess.check_output('npm --version', shell=True, encoding='UTF-8')
 
     def prepare_config(self):
         self.config['base_path'] = os.path.realpath(os.path.join(

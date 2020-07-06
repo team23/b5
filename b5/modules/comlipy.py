2148285bd77267db88cc16f258d4a2d832d4226b
Emanuel Jacob <jacob@team23.de>
2020-07-06 21:17:29 +0200

    feat(jira#8): Remove base_path, Add update command

--- b5/modules/comlipy.py
+++ b5/modules/comlipy.py
@@ -1,5 +1,4 @@
 import os
-import shlex
 
 from . import BaseModule
 
@@ -10,20 +9,14 @@ class ComlipyModule(BaseModule):
     """
 
     DEFAULT_CONFIG = {
-        'base_path': '.',
         'comlipy_bin': 'comlipy',
         'comlipy_install_bin': 'comlipy-install',
         'config_path': '',
     }
 
     def prepare_config(self):
-        self.config['base_path'] = os.path.realpath(os.path.join(
-            self.state.run_path,
-            self.config['base_path'],
-        ))
-
         if self.config['config_path']:
-            self.config['config_path'] = os.path.join(self.config['base_path'], self.config['config_path'])
+            self.config['config_path'] = os.path.realpath(os.path.join(self.state.run_path, self.config['config_path']))
 
     def get_script(self):
         script = [super(ComlipyModule, self).get_script()]
@@ -33,26 +26,32 @@ class ComlipyModule(BaseModule):
         config_param = f" -c '{self.config['config_path']}'" if self.config['config_path'] else ''
         script.append(self._script_function_source('run', '''
         (
-            cd {base_path} && \\
             {comlipy_bin}{comlipy_config_param} "$@"
         )
         '''.format(
-            name=self.name,
-            base_path=shlex.quote(self.config['base_path']),
-            comlipy_bin=self.config['comlipy_bin'],
-            comlipy_config_param=config_param,
+                name=self.name,
+                comlipy_bin=self.config['comlipy_bin'],
+                comlipy_config_param=config_param,
         )))
 
         script.append(self._script_function_source('install', '''
         (
-            cd {base_path} && \\
             {comlipy_install_bin}{comlipy_config_param}
         )
         '''.format(
-            name=self.name,
-            base_path=shlex.quote(self.config['base_path']),
-            comlipy_install_bin=self.config['comlipy_install_bin'],
-            comlipy_config_param=config_param,
+                name=self.name,
+                comlipy_install_bin=self.config['comlipy_install_bin'],
+                comlipy_config_param=config_param,
+        )))
+
+        script.append(self._script_function_source('update', '''
+            (
+                {comlipy_install_bin}{comlipy_config_param}
+            )
+            '''.format(
+                name=self.name,
+                comlipy_install_bin=self.config['comlipy_install_bin'],
+                comlipy_config_param=config_param,
         )))
 
         return '\n'.join(script)

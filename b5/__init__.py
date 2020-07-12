d1381c92951e533e9f83e99c18dd27fe8e068f3f
David Danier <danier@team23.de>
2020-07-12 18:55:33 +0200

    Preparing next version

--- b5/__init__.py
+++ b5/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-VERSION = '1.1.8'
+VERSION = '1.1.9-dev'
 B5_PATH = os.path.realpath(os.path.dirname(__file__))
 B5_BASH_PATH = os.path.join(B5_PATH, 'bash')
 B5_TEMPLATES_PATH = os.path.join(B5_PATH, 'templates')
